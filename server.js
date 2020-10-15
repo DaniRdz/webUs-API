cc12575974c86f1cd159284be846e5d89027084d
RdzCore <daniel_rod_con@hotmail.com>
2020-10-15 13:23:52 -0500

    add routes

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+
 const app = express();
 const PORT = 3000;
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.json({ Menssage: "Hello My beautiful Friends" });

06a850b615b1a3c2bcd7521245da2a365fd045c1
RdzCore <daniel_rod_con@hotmail.com>
2020-10-28 13:28:44 -0600

    fix bug of invalid email

--- app/api/controllers/users.js
+++ app/api/controllers/users.js
@@ -24,17 +24,21 @@ module.exports = {
       if (err) {
         next(err);
       } else {
-        if (bcrypt.compareSync(req.body.password, userInfo.password)) {
-          const token = jwt.sign({ id: userInfo._id }, config.SECRET, {
-            expiresIn: "1h",
-          });
-          res.json({
-            status: "ok",
-            message: "user has been authenticated!",
-            data: { user: userInfo, token: token },
-          });
-        } else {
+        if (userInfo == null) {
           res.json({ status: "error", message: "Invalid email/password!!" });
+        } else {
+          if (bcrypt.compareSync(req.body.password, userInfo.password)) {
+            const token = jwt.sign({ id: userInfo._id }, config.SECRET, {
+              expiresIn: "1h",
+            });
+            res.json({
+              status: "ok",
+              message: "user has been authenticated!",
+              data: { user: userInfo, token: token },
+            });
+          } else {
+            res.json({ status: "error", message: "Invalid email/password!!" });
+          }
         }
       }
     });

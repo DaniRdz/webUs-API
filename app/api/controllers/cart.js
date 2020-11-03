ae434baceb2f1a9e6ccc15ed888f6abe29875f91
RdzCore <daniel_rod_con@hotmail.com>
2020-11-03 14:13:19 -0600

    built products populate and calculate cartSubtotal

--- app/api/controllers/cart.js
+++ app/api/controllers/cart.js
@@ -1,14 +1,23 @@
 const cartModel = require("../models/cart");
+const products = require("../models/product");
 
 module.exports = {
   addToCart: (req, res, next) => {
     const { items } = req.body;
-    cartModel.create({ items }, (err, result) => {
-      if (err) {
-        next(err);
-      } else {
-        res.json({ status: "succes", message: "item added" });
+    products.populate(items, { path: "productId", select: "price" }, () => {
+      let subtotal = 0;
+
+      for (var i = 0; i < items.length; i++) {
+        subtotal += items[i].quantity * items[i].productId.price;
       }
+
+      cartModel.create({ items, subtotal }, (err, result) => {
+        if (err) {
+          next(err);
+        } else {
+          res.json({ status: "succes", message: "item added" });
+        }
+      });
     });
   },
 };
